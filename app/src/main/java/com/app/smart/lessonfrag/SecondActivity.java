fa067fd514605967c8ba98a10588e2c697899131
Mohamed <mohfoad@gmail.com>
2018-08-07 00:21:42 +0200

    update

--- app/src/main/java/com/app/smart/lessonfrag/SecondActivity.java
+++ app/src/main/java/com/app/smart/lessonfrag/SecondActivity.java
@@ -3,12 +3,17 @@ package com.app.smart.lessonfrag;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class SecondActivity extends AppCompatActivity{
+
+    private static final String TAG = "SecondActivity";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity_layout);
+
+        Log.d(TAG,"onCREATE: STARTED.");
     }
 }
